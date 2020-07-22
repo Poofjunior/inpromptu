7135646caa3e7d3c9f4449bda9f45aeb0f782b34
Joshua Vasquez <seeifyoucanmeetme@gmail.com>
2020-07-22 13:41:38 -0700

    updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", newline="", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="inpromptu",
-    version="0.0.1",
+    version="0.0.2",
     author="Joshua Vasquez",
     author_email="joshua@doublejumpelectric.com",
     description="An inferrable line oriented command prompt interpreter",
